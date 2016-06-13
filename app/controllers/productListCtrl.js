8b5cc96a693df544df39f3916a5681d2382fd23d
TillCollapse <victor-14@o2.pl>
2016-06-13 15:25:10 +0200

    ver. 1.0.2
    Nawigacja i zakupy

--- app/controllers/productListCtrl.js
+++ app/controllers/productListCtrl.js
@@ -2,7 +2,7 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
     .constant("productListPageCount", 3)
-    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
     var selectedCategory = null;
     
     $scope.selectedPage = 1;
@@ -27,5 +27,9 @@ angular.module("sportsStore")
     
     $scope.getPageClass = function (page) {
         return $scope.selectedPage == page ? productListActiveClass : "";
+    };
+    $scope.addPorductToCart = function (product) {
+        console.log(product.id + " " + product.name + " " + product.price);
+        cart.addProduct(product.id, product.name, product.price);
     };
 });
--- app/controllers/sportsStore.js
+++ app/controllers/sportsStore.js
@@ -3,14 +3,25 @@
     //modul
     var sportStore = angular.module("sportsStore");
     
-    sportStore.controller("sportsStoreCtrl", function ($scope) {
-        $scope.data = {
+    sportStore
+        .constant("dataUrl", "http://localhost:5000/products/")
+        .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+        $scope.data = {};
+        $http.get(dataUrl)
+            .success(function (data) {
+                $scope.data.products = data;
+            })
+            .error(function (error) {
+                $scope.data.error = error;
+            });
+        
+        /*$scope.data = {
             products: [
                 { name: "Produkt #1", description: "To jest produkt", category: "Kategiria #1", price: 100 },
                 { name: "Produkt #2", description: "To jest produkt", category: "Kategiria #2", price: 100 },
                 { name: "Produkt #3", description: "To jest produkt", category: "Kategiria #3", price: 10 },
                 { name: "Produkt #4", description: "To jest produkt", category: "Kategiria #4", price: 100 }
             ]
-        };
+        };*/
     });
 }());
